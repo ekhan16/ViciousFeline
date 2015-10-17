262c9b2f924ed3d9cce3fcbab79519c7f20bef6d
Ekhan16 <ekhan15.sg@gmail.com>
2015-10-17 16:32:07 -0700

    change to structure of data pushed to firebase

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+v2c1s1 = File.open("public/storyline/page1.html", 'rb') {|io| io.read }
+
+
+base_uri = "https://viciousunicorn.firebaseio.com/Sagas"
+firebase = Firebase::Client.new(base_uri)
+
+firebase.push(base_uri, {"Volume 1" => {"Contents" => {"Chapter 1"=>{"Scene 1"=>"eKhan was impressed by my coolness", "Scene 2"=>"asdfasdfasdf"}}}})
+
+firebase.push(base_uri, {"Volume 2" => {"Contents" => {"Chapter 1"=>{"Scene 1"=>v2c1s1, "Scene 2"=>"Get Fucked", "Scene 3"=>"When you think you've got it, but turns out you really don't"}}}})
+
